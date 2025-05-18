700bf2db13d047d2acb3cfa01cdf578fee5fb493
Buicongbang04 <buicongbang1010@gmail.com>
2025-05-18 13:15:46 +0700

    abc

--- Server/server.js
+++ Server/server.js
@@ -1,28 +1,36 @@
+import express from 'express';
 import cookieParser from 'cookie-parser';
-import express, { json } from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
-import 'dotenv/config'
+import 'dotenv/config.js';
 import userRouter from './routes/userRoute.js';
+
 
 const app = express();
 const port = process.env.PORT || 4000;
-await connectDB()
 
-// Allow multiple origins
-const allowedOrigins = ["http://localhost:5173/"]
+await connectDB();
 
-// Middleware config
-app.use(express,json);
+const allowedOrigins = [
+  'http://localhost:5173',
+]
+
+// Middleware configuration
+app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: allowedOrigins,
-    credentials: true
-}))
+  origin: allowedOrigins,
+  credentials: true,
+}));
 
-app.get('/', (req, res) => res.send("API is working"));
-app.use('/api/user', userRouter)
+app.get('/', (req, res) => {
+  res.send('API is running...');
+}
+);
+app.use('/api/user', userRouter);
+
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+}
+);
