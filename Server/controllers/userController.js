60dba7928e3b68775db3697bdfd27de25892461d
Buicongbang04 <buicongbang1010@gmail.com>
2025-05-18 13:52:33 +0700

    auth user and logout

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -103,3 +103,42 @@ export const login = async (req, res) => {
         })
     }
 }
+
+// Check Auth: /api/user/is-auth
+export const isAuth = async (req, res) => {
+    try{
+        const {userId} = req.body;
+        const user = await User.findById(userId).select('-password -cartItems -__v')
+        return res.json({
+            success: true,
+            user
+        })
+    }catch(error){
+        console.log(error.message)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+// Logout user: /api/user/logout
+export const logout = async (req, res) => {
+    try{
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        return res.json({
+            success: true,
+            message: 'Logged out successfully'
+        })
+    }catch(error){
+        console.log(error.message)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
