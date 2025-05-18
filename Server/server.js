ebf1a0bd9cc7124b13968d51a5ace7e8b57a12c6
Buicongbang04 <buicongbang1010@gmail.com>
2025-05-18 11:18:54 +0700

    register

--- Server/server.js
+++ Server/server.js
@@ -3,6 +3,7 @@ import express, { json } from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config'
+import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,6 +21,7 @@ app.use(cors({
 }))
 
 app.get('/', (req, res) => res.send("API is working"));
+app.use('/api/user', userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
