59fd0c5f9cd4549e5cba4537c73b514615d78232
Buicongbang04 <buicongbang1010@gmail.com>
2025-05-18 13:35:40 +0700

    login

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -50,3 +50,56 @@ export const register = async (req, res) => {
         })
     }
 }
+
+// Login user: api/user/login
+export const login = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        if (!email || !password){
+            return res.json({
+                success: false,
+                message: "Email and password are required!"
+            })
+        }
+
+        const user = await User.findOne({email})
+        if(!user){
+            return res.json({
+                success: false,
+                message: 'User not found'
+            })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.json({
+                success: false,
+                message: 'Password is incorrect'
+            })
+        }
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
+        res.cookie('token', token, {
+            httpOnly: true, // prevent JavaScript to access cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF production
+            maxAge: 7 * 24 * 60 * 60 * 1000, // cookie expiration time
+        })
+
+        return res.json({
+            success: true,
+            user: {
+                email: user.email,
+                name: user.name
+            }
+        })
+    }catch(error){
+        console.log(error.message)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
